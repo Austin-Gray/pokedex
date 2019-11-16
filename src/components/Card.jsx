463ed52665f8a79da7086751b2c5cfd3d21ec0d6
Austin <austin.gray4292@gmail.com>
2019-11-15 21:11:39 -0800

    Adds pokeball image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Card = props => {
   return (
     <div>
+      <img src='assets/pokeball.png' className='pokeball'/>
       <Link to='/detail' className='link' name={props.name} onClick={props.selectPokemon}>{props.capitalize(props.name)}</Link>
       <h5 className='mt-1 mb-1'># {props.number}</h5>
     </div>
